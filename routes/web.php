e708943a9cd5c8e5ba39c278d8f5786f4a8cf868
tynevosa <michael.edy623@gmail.com>
2022-07-26 20:52:08 +0200

    template booking

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('index');});
+Route::get('/main', function () {return view('main');});
+Route::get('/rooms', function () {return view('rooms');});
+Route::get('/room-details', function () {return view('room-details');});
+Route::get('/contact', function () {return view('contact');});
+Route::get('/blog', function () {return view('blog');});
+Route::get('/blog-details', function () {return view('blog-detail');});
+Route::get('/about-us', function () {return view('about-us');});
